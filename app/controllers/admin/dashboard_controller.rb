a84917e64ccf3262946b1779e54cb3b1bde9ef3c
EavanK <eavan.hsk@gmail.com>
2022-01-06 12:00:07 -0800

    modify instance variables name

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_BASIC_USER'], password: ENV['HTTP_BASIC_PASSWORD']
 
   def show
-    @products = Product.count
-    @categories = Category.count
+    @products_count = Product.count
+    @categories_count = Category.count
   end
 end
