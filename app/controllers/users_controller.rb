9e883edfec22279330bce2aede66472156c7b0d6
EavanK <eavan.hsk@gmail.com>
2022-01-07 02:45:20 -0800

    add error handling for sign up form validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,16 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
 
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to [:root]
     else
-      flash[:warning] = 'Failed: Check your form again'
-      redirect_to new_user_path
+      flash[:warning] = @user.errors.messages
+      redirect_to [:new, :user]
     end
   end
 
@@ -21,5 +21,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
-  
+
 end
